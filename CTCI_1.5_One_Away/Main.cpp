8743141171d4692a527f0f9d7846c31a302e5806
Christopher Sefcik <cdsefcik@gmail.com>
2026-02-13 21:36:28 -0500

    Add files via upload

---
+++ CTCI_1.5_One_Away/Main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+/*
+*There are three types of edits that can be performed on strings: Insert, remove or replace. Given two strings, write a function to check if they
+* are one edit, or zero edits away. 
+* EXE:
+* pale, ple -> true
+* pales, pale -> true
+* pale, bale -> true
+* pale, bake -> false
+*/
